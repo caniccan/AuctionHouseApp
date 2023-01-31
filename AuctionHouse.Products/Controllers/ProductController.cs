8965ca5a472df562bb0ec648e3ca010a7c0c98c6
Can İçcan <95756970+caniccan@users.noreply.github.com>
2023-01-31 21:51:02 +0300

    Added ProductController.

---
+++ AuctionHouse.Products/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AuctionHouse.Products.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
