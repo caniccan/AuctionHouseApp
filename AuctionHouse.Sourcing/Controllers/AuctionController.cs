d3054c073a82c37ae5e3595435c422c04b69691a
Can İçcan <95756970+caniccan@users.noreply.github.com>
2023-02-04 15:46:19 +0300

    Sourcing's project files have been added.

---
+++ AuctionHouse.Sourcing/Controllers/AuctionController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AuctionHouse.Sourcing.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class AuctionController : ControllerBase
+    {
+        
+    }
+}
