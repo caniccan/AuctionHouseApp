e17ea126f1eb5c62d16fefbe7cedbd661f50f61e
Can İçcan <95756970+caniccan@users.noreply.github.com>
2023-02-05 13:55:33 +0300

    Entities Added.

---
+++ AuctionHouse.Sourcing/Entities/Bid.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace AuctionHouse.Sourcing.Entities
+{
+    public class Bid
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string AuctionId { get; set; }
+        public string ProductId { get; set; }
+        public string SellerUserName { get; set; }
+        public string Price { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
