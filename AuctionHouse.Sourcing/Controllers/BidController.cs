1605dfcfc2ce28e65b6141b20fa28f6c359b053c
Can İçcan <95756970+caniccan@users.noreply.github.com>
2023-02-10 18:20:48 +0300

    Added CRUD actions in BidController.

--- AuctionHouse.Sourcing/Controllers/BidController.cs
+++ AuctionHouse.Sourcing/Controllers/BidController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AuctionHouse.Sourcing.Entities;
+using AuctionHouse.Sourcing.Repositories.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
 
 namespace AuctionHouse.Sourcing.Controllers
 {
@@ -7,5 +9,39 @@ namespace AuctionHouse.Sourcing.Controllers
     [ApiController]
     public class BidController : ControllerBase
     {
+        private readonly IBidRepository _bidRepository;
+
+        public BidController(IBidRepository bidRepository)
+        {
+            _bidRepository = bidRepository;
+        }
+
+        [HttpPost]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        public async Task<ActionResult> SendBid([FromBody] Bid bid)
+        {
+            await _bidRepository.SendBid(bid);
+            return Ok();
+        }
+
+
+        [HttpGet("GetBidByActionId")]
+        [ProducesResponseType(typeof(IEnumerable<Bid>),(int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<Bid>>> GetBidByActionId(string id)
+        {
+            IEnumerable<Bid> bids= await _bidRepository.GetBidsByAuctionId(id);
+            return Ok(bids);
+        }
+
+
+        [HttpGet("GetWinnerBid")]
+        [ProducesResponseType(typeof(Bid), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Bid>> GetWinnerBid(string id)
+        {
+            Bid bid= await _bidRepository.GetWinnerBid(id);
+            return Ok(bid);
+        }
+
+
     }
 }
