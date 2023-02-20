0080288b0161b78ac126d9947ba8f8de0caaeaa4
Can İçcan <95756970+caniccan@users.noreply.github.com>
2023-02-20 15:52:03 +0300

    Added MigrationManager extension class and Seed data.

---
+++ AuctionHouse.Order/Extensions/MigrationManager.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Infrastructure.Data;
+
+namespace AuctionHouse.Order.Extensions
+{
+    public static class MigrationManager
+    {
+        public static IHost MigrateDatabase(this IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                try
+                {
+                    var orderContext = scope.ServiceProvider.GetRequiredService<OrderContext>();
+
+                    if (orderContext.Database.ProviderName != "Microsoft.EntityFrameworkCore.InMemory")
+                    {
+                        orderContext.Database.Migrate();
+                    }
+
+                    OrderContextSeed.SeedAsync(orderContext).Wait();
+                }
+                catch (Exception exception)
+                {
+
+                    throw;
+                }
+            }
+
+            return host;
+        }
+    }
+}
--- AuctionHouse.Order/Program.cs
+++ AuctionHouse.Order/Program.cs
@@ -1,12 +1,19 @@
+using AuctionHouse.Order.Extensions;
+using Ordering.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 
+builder.Services.AddInfrastructure(builder.Configuration);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+app.MigrateDatabase();
 
 app.UseHttpsRedirection();
 
