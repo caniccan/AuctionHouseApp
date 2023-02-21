60ca7018358c9555b17f17db9e8fe967ae51645d
Can İçcan <95756970+caniccan@users.noreply.github.com>
2023-02-21 11:20:04 +0300

    Added OrderRepository.

---
+++ Ordering.Infrastructure/Repositories/OrderRepository.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Domain.Entities;
+using Ordering.Domain.Repositories;
+using Ordering.Infrastructure.Data;
+using Ordering.Infrastructure.Repositories.Base;
+
+namespace Ordering.Infrastructure.Repositories
+{
+    public class OrderRepository : Repository<Order>, IOrderRepository
+    {
+        public OrderRepository(OrderContext dbContext): base(dbContext)
+        {
+            
+        }
+
+        public async Task<IEnumerable<Order>> GetOrdersBySellerUserName(string userName)
+        {
+            var orderList = await _dbContext.Orders
+                .Where(x => x.SellerUserName == userName).ToListAsync();
+            return orderList;
+        }
+    }
+}
