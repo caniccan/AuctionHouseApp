93a202c29b143349bbd47dcf6083b7546fcb4d3d
Can İçcan <95756970+caniccan@users.noreply.github.com>
2023-02-22 18:04:21 +0300

    Added order command and validator class.

---
+++ Ordering.Application/Commands/OrderCreate/OrderCreateValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace Ordering.Application.Commands.OrderCreate
+{
+    public class OrderCreateValidator : AbstractValidator<OrderCreateCommand>
+    {
+        public OrderCreateValidator()
+        {
+            RuleFor(x => x.SellerUserName)
+                .EmailAddress()
+                .NotEmpty();
+
+            RuleFor(x => x.ProductId)
+                .NotEmpty();
+        }
+    }
+}
