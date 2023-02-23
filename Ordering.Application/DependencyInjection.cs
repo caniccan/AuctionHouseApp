0dc9082238ce846f5c31e9ea048f5ac94097f43f
Can İçcan <95756970+caniccan@users.noreply.github.com>
2023-02-23 18:18:34 +0300

    Added order create handler and mapping profile.

--- Ordering.Application/DependencyInjection.cs
+++ Ordering.Application/DependencyInjection.cs
@@ -1,6 +1,8 @@
-﻿using FluentValidation;
+﻿using AutoMapper;
+using FluentValidation;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
+using Ordering.Application.Mapper;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,6 +19,18 @@ namespace Ordering.Application
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
             services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
             services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            #region Configure Mapper
+
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.ShouldMapProperty = p => p.GetMethod.IsPublic || p.GetMethod.IsAssembly;
+                cfg.AddProfile<OrderMappingProfile>();
+            });
+            var mapper=config.CreateMapper();
+
+            #endregion
+
             return services;
         }
     }
