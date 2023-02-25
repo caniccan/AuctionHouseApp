60a1663dbe057d2f8d20cfd7c41b4e3af310325b
Can İçcan <95756970+caniccan@users.noreply.github.com>
2023-02-25 13:18:03 +0300

    Added OrderController's actions.

--- AuctionHouse.Order/Controllers/OrderController.cs
+++ AuctionHouse.Order/Controllers/OrderController.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Ordering.Application.Commands.OrderCreate;
+using Ordering.Application.Queries;
+using Ordering.Application.Responses;
+using System.Net;
 
 namespace AuctionHouse.Order.Controllers
 {
@@ -7,5 +12,36 @@ namespace AuctionHouse.Order.Controllers
     [ApiController]
     public class OrderController : ControllerBase
     {
+        private readonly IMediator _mediator;
+        private readonly ILogger<OrderController> _logger;
+
+        public OrderController(IMediator mediator, ILogger<OrderController> logger)
+        {
+            _mediator = mediator;
+            _logger = logger;
+        }
+
+        [HttpGet("GetOrdersByUserName/{userName}")]
+        [ProducesResponseType(typeof(IEnumerable<OrderResponse>),(int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        public async Task<ActionResult<IEnumerable<OrderResponse>>> GetOrdersByUserName(string userName)
+        {
+            var query= new GetOrdersBySellerUserNameQuery(userName);
+
+            var orders = await _mediator.Send(query);
+            if (Equals(orders.Count(),decimal.Zero))
+            {
+                return NotFound();
+            }
+            return Ok(orders);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(OrderResponse), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<OrderResponse>> OrderCreate([FromBody] OrderCreateCommand command)
+        {
+            var result= await _mediator.Send(command);
+            return Ok(result);
+        }
     }
 }
