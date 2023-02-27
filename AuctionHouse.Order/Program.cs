9f3f0e15670a8ce355857ad30c7a498daf3ba355
Can İçcan <95756970+caniccan@users.noreply.github.com>
2023-02-27 12:42:27 +0300

    Added order consumer.

--- AuctionHouse.Order/Program.cs
+++ AuctionHouse.Order/Program.cs
@@ -1,7 +1,11 @@
 using AuctionHouse.Order.Extensions;
+using EventBusRabbitMQ.Producer;
+using EventBusRabbitMQ;
 using Microsoft.OpenApi.Models;
 using Ordering.Application;
 using Ordering.Infrastructure;
+using RabbitMQ.Client;
+using AuctionHouse.Order.Consumers;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -30,6 +34,40 @@ builder.Services.AddSwaggerGen(c =>
 
 #endregion
 
+#region EventBus
+
+builder.Services.AddSingleton<IRabbitMQPersistentConnection>(sp =>
+{
+    var logger = sp.GetRequiredService<ILogger<DefaultRabbitMQPersistentConnection>>();
+    var factory = new ConnectionFactory()
+    {
+        HostName = builder.Configuration["EventBus:Hostname"]
+    };
+
+    if (!string.IsNullOrWhiteSpace(builder.Configuration["EventBus:UserName"]))
+    {
+        factory.UserName = builder.Configuration["EventBus:UserName"];
+    }
+
+    if (!string.IsNullOrWhiteSpace(builder.Configuration["EventBus:Password"]))
+    {
+        factory.UserName = builder.Configuration["EventBus:Password"];
+    }
+
+    var retryCount = 5;
+
+    if (!string.IsNullOrWhiteSpace(builder.Configuration["EventBus:RetryCount"]))
+    {
+        retryCount = int.Parse(builder.Configuration["EventBus:RetryCount"]);
+    }
+
+    return new DefaultRabbitMQPersistentConnection(factory, retryCount, logger);
+
+});
+
+builder.Services.AddSingleton<EventBusOrderCreateConsumer>();
+#endregion
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -41,6 +79,8 @@ if (app.Environment.IsDevelopment())
 // Configure the HTTP request pipeline.
 
 app.MigrateDatabase();
+
+app.UseRabbitListener();
 
 app.UseHttpsRedirection();
 
