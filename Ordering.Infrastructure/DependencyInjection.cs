03bd41b79ada4d8c53cd633de43e5241d4c4c30b
Can İçcan <95756970+caniccan@users.noreply.github.com>
2023-02-28 17:04:53 +0300

    SQL server connection settings have been made and migration has been added.

--- Ordering.Infrastructure/DependencyInjection.cs
+++ Ordering.Infrastructure/DependencyInjection.cs
@@ -18,7 +18,12 @@ namespace Ordering.Infrastructure
     {
         public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
         {
-            services.AddDbContext<OrderContext>(options => options.UseInMemoryDatabase(databaseName: "InMemoryDb"), ServiceLifetime.Singleton, ServiceLifetime.Singleton);
+            //services.AddDbContext<OrderContext>(options => options.UseInMemoryDatabase(databaseName: "InMemoryDb"), ServiceLifetime.Singleton, ServiceLifetime.Singleton);
+
+            services.AddDbContext<OrderContext>(options =>
+                    options.UseSqlServer(
+                        configuration.GetConnectionString("OrderConnection"),
+                        b => b.MigrationsAssembly(typeof(OrderContext).Assembly.FullName)), ServiceLifetime.Singleton);
 
 
             //Add Repositories
