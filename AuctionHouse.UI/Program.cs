f7523b268bd1dae984128741dcb6f2b5ba7a6566
Can İçcan <95756970+caniccan@users.noreply.github.com>
2023-03-02 12:19:21 +0300

    The layout was created and ready-made template files were added.

--- AuctionHouse.UI/Program.cs
+++ AuctionHouse.UI/Program.cs
@@ -1,6 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddMvc();
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
@@ -19,6 +21,10 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.MapRazorPages();
 
