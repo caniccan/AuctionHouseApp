3ccb3f8fe377db4867c35dca6f8f20e68de80323
Can İçcan <95756970+caniccan@users.noreply.github.com>
2023-03-05 12:11:10 +0300

    The Auction's Index page has been added.

--- AuctionHouse.UI/Controllers/AuctionController.cs
+++ AuctionHouse.UI/Controllers/AuctionController.cs
@@ -1,14 +1,15 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using AuctionHouse.UI.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AuctionHouse.UI.Controllers
 {
-    [Authorize]
     public class AuctionController : Controller
     {
         public IActionResult Index()
         {
-            return View();
+            List<AuctionViewModel> viewModelList = new List<AuctionViewModel>();
+            return View(viewModelList);
         }
     }
 }
